69df8aaba1039c6f811f290926c95b7e31acdfd0
Alvin Ng <ngalvin93@gmail.com>
2020-01-29 22:53:28 -0600

    first page

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        )
+    }
+}
+
+// const index = props => {
+//     return(
+//         <React.Fragment>
+//             <p>Hello!</p>
+//         </React.Fragment>
+//     )
+// }
+
+export default index
