764918b056f9b0920c2540c9ee3c15d1aafba639
Alvin Ng <ngalvin93@gmail.com>
2020-01-30 23:55:43 -0600

    styled components init

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import Meta from './Meta'
+// For using styled components
+// import styled from 'styled-components'
+
+// Here you define your styled component then use in the return()
+// const MyButton = styled.button`
+//     background: red;
+//     font-size: 100px;
+// `
 
 export default class Page extends Component {
     render() {
@@ -12,4 +20,4 @@ export default class Page extends Component {
             </div>
         )
     }
-}
+} 
